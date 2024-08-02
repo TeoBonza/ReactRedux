3dade66abdfc6330bd3c47ce6e560bcb6738ab9d
Teo <matteo@peculiarventures.com>
2024-08-02 10:27:03 +0300

    chore(todo): simple todo list with css

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,59 +1,114 @@
-import React from 'react';
+import React from 'react'
 import './App.css'
 
-interface HelloUsersProps {
-  names: string[];
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
 }
 
-interface HelloUsersState {
-  currentNameIndex: number;
+interface TodoAppState {
+  todos: Todo[];
+  inputValue: string;
 }
 
-class HelloUsers extends React.Component<HelloUsersProps, HelloUsersState> {
-  private interval: number | undefined;
-
-  constructor(props: HelloUsersProps) {
+class TodoApp extends React.Component<Record<string, never>, TodoAppState> {
+  constructor(props: Record<string, never>) {
     super(props);
     this.state = {
-      currentNameIndex: 0,
+      todos: [],
+      inputValue: '',
     };
   }
 
   componentDidMount() {
-    console.log('componentDidMount');
-    this.interval = window.setInterval(this.cycleNames, 5000);
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  componentDidUpdate(prevProps: HelloUsersProps, prevState: HelloUsersState) {
-    console.log('componentDidUpdate');
-  }
-
-  componentWillUnmount() {
-    console.log('componentWillUnmount');
-    if (this.interval !== undefined) {
-      clearInterval(this.interval);
+    const savedTodos = localStorage.getItem('todos');
+    if (savedTodos) {
+      this.setState({ todos: JSON.parse(savedTodos) });
     }
   }
 
-  cycleNames = () => {
+  componentDidUpdate(prevProps: Record<string, never>, prevState: TodoAppState) {
+    if (prevState.todos !== this.state.todos) {
+      localStorage.setItem('todos', JSON.stringify(this.state.todos));
+    }
+  }
+
+  handleAddTodo = () => {
+    if (this.state.inputValue.trim() === '') return;
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: this.state.inputValue,
+      completed: false,
+    };
+
     this.setState((prevState) => ({
-      currentNameIndex: (prevState.currentNameIndex + 1) % this.props.names.length,
+      todos: [...prevState.todos, newTodo],
+      inputValue: '',
     }));
   };
 
-  render() {
-    console.log('render');
-    const { names } = this.props;
-    const { currentNameIndex } = this.state;
-    const name = names[currentNameIndex];
+  handleDeleteTodo = (id: number) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((todo) => todo.id !== id),
+    }));
+  };
 
+  handleDeleteAllTodos = () => {
+    this.setState({ todos: [] });
+  };
+
+  handleToggleComplete = (id: number) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      ),
+    }));
+  };
+
+  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  render() {
     return (
-      <div>
-        <h1>Hello {name}!</h1>
+      <div className="todo-container">
+        <input
+          type="text"
+          className="todo-input"
+          placeholder="Input TODO"
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+        />
+        <div className="todo-buttons">
+          <button onClick={this.handleAddTodo}>Add</button>
+          <button onClick={this.handleDeleteAllTodos}>Delete all</button>
+        </div>
+        <div>
+          {this.state.todos.map((todo) => (
+            <div className="todo-item" key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => this.handleToggleComplete(todo.id)}
+              />
+              <span
+                style={{
+                  textDecoration: todo.completed ? 'line-through' : 'none',
+                }}
+              >
+                {todo.text}
+              </span>
+              <button onClick={() => this.handleDeleteTodo(todo.id)}>
+                Delete
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
 }
 
-export default HelloUsers;
+export default TodoApp;
